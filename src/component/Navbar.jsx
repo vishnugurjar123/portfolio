a48915c9f97bba8a54e7dd7e64004bd3867a55b8
jbmr gurjar <gurjarvishnu740@gmail.com>
2025-01-19 18:05:11 +0530

    updated

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,42 +4,66 @@ import{Link,NavLink} from'react-router-dom';
 import img from './Vidoes/photo.jpg'
 import "./Navbar.css";
 const Navbar = () => {
-  
   const [menuOpen, setMenuOpen] = useState(false);
-
   return (
-    <nav className='shadow flex  sticky z-30 top-0 h-full w-full'>
-      <Link to="/" className="title navbar-brand font-mono font-bold text-xl  text-cyan-950 ">
-        VISHNU GURJAR
-      </Link>
-      <button className=" menu navbar-toggler" onClick={() => setMenuOpen(!menuOpen)} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    
+
+  <nav className="navbar navbar-expand-lg  bg-white shadow flex sticky z-30 top-0 h-full w-full ">
+    <div className="container-fluid">
+      <Link className="navbar-brand font-mono font-bold text-xl  text-cyan-950" to="/">VISHNU GURJAR</Link>
+      <button className=" menu navbar-toggler" onClick={() => setMenuOpen(!menuOpen)} type="button">
      
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+         <span></span>
+         <span></span>
+         <span></span>
+         <span></span>
   
-      </button>
-      {/* <ul className={menuOpen ? "open" : ""}> */}
-      <div className="collapse navbar-collapse font-mono text-xl h-100 d-flex justify-content-end  " id="navbarNav">
-      <ul className={menuOpen ? "open" : ""}   >
-           <li className="nav-item  hover:text-orange-100">
-           <Link className="nav-link " aria-current="page" to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/Project">Project</Link>
-           </li>
-           <li className="nav-item">
-             <Link className="nav-link" to="/About">About</Link>
-           </li>
+       </button>
+      <div className="collapse navbar-collapse font-mono text-xs h-100 d-flex justify-content-end  " id="navbarNav">
+        <ul className={menuOpen ? "open" : ""}>
+          <li className="nav-item font-mono   hover:text-orange-100">
+            <Link className="nav-link " aria-current="page" to="/">Home</Link>
+          </li>
+          <li className="nav-item font-mono ">
+            <Link className="nav-link" to="/Project">Project</Link>
+          </li>
           <li className="nav-item">
-           <Link className="nav-link" to="/Contact">Contact</Link>
+            <Link className="nav-link" to="/About">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Contact">Contact</Link>
           </li>
 
-         </ul>
-         </div>
+        </ul>
+      </div>
+      <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-7  bg-cover rounded-full">
+          <img style={{ width:"30px, bacground-cover"}}
+            alt=" CSS Navbar component"
+            src={img} />
+        </div>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100  text-sm rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li>
+          <Link to="http://localhost:5175/ " className="justify-between text-sm" target="_blank">
+             WebSite
+            <span className="badge text-xs">Personal</span>
+          </Link>
+        </li>
+        <Link to="#">Settings</Link>
+        <Link to="#">Logout</Link>
+      </ul>
+    </div>
+    </div>
+  
+  </nav>
 
-    </nav>
+
+
+  
   )
 }
 
